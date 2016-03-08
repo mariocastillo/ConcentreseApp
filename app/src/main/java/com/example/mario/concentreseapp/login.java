b3fefad35b06667df0fef16b5f1ab310c9b18903
Mario Castillo <marioe.castillo93@gmail.com>
2016-03-08 00:09:18 -0500

    boton de reset en el juego y traduccion al ingles

--- app/src/main/java/com/example/mario/concentreseapp/login.java
+++ app/src/main/java/com/example/mario/concentreseapp/login.java
@@ -8,6 +8,9 @@ import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +19,8 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.mario.concentreseapp.Player;
 
 import java.util.ArrayList;
@@ -34,7 +39,7 @@ public class login extends AppCompatActivity {
         LinearLayout linear = (LinearLayout) findViewById(R.id.login);
         TextView newp = new TextView(this);
         EditText newpe = new EditText(this);
-        newp.setText("Nombre");
+        newp.setText(R.string.name);
         newpe.setId(0);
         linear.addView(newp);
         linear.addView(newpe);
@@ -44,13 +49,42 @@ public class login extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id==R.id.creadores) {
+            Toast toast1 = Toast.makeText(getApplicationContext(), "Mario Castillo Ramirez"+"\n"+"marioe.castillo93@gmail.com", Toast.LENGTH_SHORT);
+            toast1.setGravity(Gravity.CENTER,0,0);
+            toast1.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
     public void añadir(View view) {
         count++;
         if (count<=4) {
             LinearLayout linear = (LinearLayout) findViewById(R.id.login);
             TextView newp = new TextView(this);
             EditText newpe = new EditText(this);
-            newp.setText("Nombre");
+            newp.setText(R.string.name);
             newpe.setId(count);
             linear.addView(newp);
             linear.addView(newpe);
